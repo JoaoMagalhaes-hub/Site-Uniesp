a02900d9e993606b35375dffa605fe1e7e685e53
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2025-05-07 20:59:01 -0300

    Sou um garoto de programa :)

---
+++ site-uniesp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Inicial from './pages/Inicial'
+
+const App = () => {
+  return (
+    <div>
+        
+        <Inicial />
+        
+    </div>
+  )
+}
+
+export default App
